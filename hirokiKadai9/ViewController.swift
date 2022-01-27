2df76be92ad177f2736abc206a4d7544eaf21664
hecuhecu <hecunyuji5@gmail.com>
2022-01-27 09:29:33 +0900

    Initial Commit

---
+++ hirokiKadai9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hirokiKadai9
+//
+//  Created by 河村宇記 on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
